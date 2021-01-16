7559ade4869f65df8bc7c0f03da420e54f7b9a82
TeeFiX42 <47397709+TeeFiX42@users.noreply.github.com>
2021-01-16 13:42:29 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,14 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("" + (input.temperature()))
 })
 input.onButtonPressed(Button.B, function () {
+	
+})
+basic.forever(function () {
     led.plotBarGraph(
     input.temperature(),
     25
     )
+    if (input.temperature() <= 5) {
+        music.playMelody("A F A F A F A F ", 120)
+    }
 })
